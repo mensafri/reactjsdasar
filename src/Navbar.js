99b012ae16dc5685059ac02a16569c6d4c4b1193
mensafri <safrijawad1245@gmail.com>
2023-04-11 13:25:30 +0700

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,32 @@
+// import React from 'react'
+
+// export default function Navbar() {
+//   return (
+//     <div>
+//         <h2>Navbar</h2>
+//     </div>
+//   )
+// }
+
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Navbar</h2>
+    </div>
+    )
+  }
+}
+
+// import React from 'react'
+
+// export const Navbar = () => {
+//   return (
+//     <div>
+//         <h2>Navbar</h2>
+//     </div>
+//   )
+// }
+
